eda75e28a40fab7c79ae54d7f0b4fb02c20a7a57
Shair Azam <shair.yasini@gmail.com>
2024-10-14 00:27:31 -0700

    cmmit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// function shair(){
+//      return ["shair" , 22, "gilgit"];
+
+// }
+  // let [name , age, city] = shair();
+  // console.log(name);
+  // console.log(age);
+  // console.log(city);
+
+        // Arrow function is define here 
+  let name = "Hello"
+ abc = ()=>{
+   alert(`shair your function is running here.. ${name}`)
+  //  document.write(`shair your function is running here.. ${name}`)
+}
+abc();
